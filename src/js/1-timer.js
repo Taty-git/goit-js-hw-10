1bd9d945f7fd2e96c223702caa6d9dac5ae722c2
Taty <taty.bystrova@gmail.com>
2025-03-10 17:32:18 +0200

    start js with input, task-1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,3 +4,28 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
+const inputData = document.querySelector("#datetime-picker");
+const btnStart = document.querySelector("[data-start]");
+
+let userSelectedDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr("#datetime-picker", options);
+
+btnStart.addEventListener("click", userSelectedDate => {
+    userSelectedDate = new Date(inputData.value);
+    if (userSelectedDate < new Date()) {
+        window.alert("Please choose a date in the future");
+        btnStart.disabled = true;
+        return;
+    } else {
+        btnStart.disabled = false;
+    }
+});
